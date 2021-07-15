bb8d9816e0f00eba5ab52617285e9ff6a488c759
jancoding <shahjanvir@gmail.com>
2021-07-15 15:27:36 -0700

    work in progress bonAPI request using volley

--- app/src/main/java/com/example/milkandcookies/activities/LoginActivity.java
+++ app/src/main/java/com/example/milkandcookies/activities/LoginActivity.java
@@ -9,22 +9,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+//
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.milkandcookies.Ingredient;
 import com.example.milkandcookies.R;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+//import okhttp3.Request;
+//import okhttp3.RequestBody;
+//import okhttp3.Response;
+import okhttp3.internal.http2.Header;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -68,32 +83,70 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void BonAPITest() {
-        final MediaType JSON
-                = MediaType.parse("application/json; charset=utf-8");
-        OkHttpClient client = new OkHttpClient();
-        RequestBody body = RequestBody.create(JSON, "{\"ingredients\": ['250gr white wheat flour', '50ml cow milk', '1 chicken breast', '0.5 cups of white rice']}");
-        Request request = new Request.Builder()
-                .header("Authorization", "Authorization: Token 50b313b1e22fa05eb8512f6f78845d8f5ec8f4b7")
-                .header("Content-type", "application/json")
-                .url("https://www.bon-api.com/api/v1/ingredient/alternatives")
-                .post(body)
-                .build();
 
-        client.newCall(request).enqueue(new Callback() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest sr = new StringRequest(Request.Method.POST,"https://www.bon-api.com/api/v1/ingredient/alternatives", new Response.Listener<String>() {
             @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
+            public void onResponse(String response) {
+                Log.d("bonapi", response.toString());
             }
+        }, new Response.ErrorListener() {
             @Override
-            public void onResponse(Call call, final Response response) throws IOException {
-                if (!response.isSuccessful()) {
-                    throw new IOException("Unexpected code " + response);
-                } else {
-                    // do something wih the result
-                    Log.d("bonapi", response.toString());
-                }
+            public void onErrorResponse(VolleyError error) {
+                Log.d("bonapi error", error.toString());
             }
-        });
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                return params;
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                String body = "{\"ingredients\": ['250gr white wheat flour', '50ml cow milk', '1 chicken breast', '0.5 cups of white rice']}";
+                return body.getBytes();
+            }
+
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/json");
+                params.put("Authorization","50b313b1e22fa05eb8512f6f78845d8f5ec8f4b7");
+                return params;
+            }
+        };
+        queue.add(sr);
+
+//        final MediaType JSON
+//                = MediaType.parse("application/json; charset=utf-8");
+//        OkHttpClient client = new OkHttpClient();
+//        RequestBody body = RequestBody.create(JSON, "{\"ingredients\": ['250gr white wheat flour', '50ml cow milk', '1 chicken breast', '0.5 cups of white rice']}");
+//
+//        Request request = new Request.Builder()
+//                .addHeader("Authorization", "155d4413de0c8b61612fabc4a4f501621e951160")
+//                .addHeader("Content-type", "application/json")
+//                .url("https://www.bon-api.com/api/v1/ingredient/alternatives")
+//                .post(body)
+//                .build();
+//
+//
+//        client.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                e.printStackTrace();
+//            }
+//            @Override
+//            public void onResponse(Call call, final Response response) throws IOException {
+//                if (!response.isSuccessful()) {
+//                    throw new IOException("Unexpected code " + response);
+//                } else {
+//                    // do something wih the result
+//                    Log.d("bonapi", response.body().toString());
+//                }
+//            }
+//        });
 
     }
 
