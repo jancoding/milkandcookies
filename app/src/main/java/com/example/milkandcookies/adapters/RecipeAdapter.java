259ebe2aea0e1e2c2579ba91660036324d28b55d
jancoding <shahjanvir@gmail.com>
2021-07-15 19:20:05 -0700

    remove recipes:

--- app/src/main/java/com/example/milkandcookies/adapters/RecipeAdapter.java
+++ app/src/main/java/com/example/milkandcookies/adapters/RecipeAdapter.java
@@ -2,19 +2,26 @@ package com.example.milkandcookies.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.milkandcookies.Ingredient;
 import com.example.milkandcookies.Recipe;
+import com.example.milkandcookies.activities.DetailActivity;
 import com.example.milkandcookies.activities.ReplaceActivity;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.Serializable;
@@ -51,13 +58,50 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder
         return recipes.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView tvRecipe;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvRecipe = itemView.findViewById(android.R.id.text1);
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    deleteRecipe();
+                    return true;
+                }
+            });
+        }
+
+        private void deleteRecipe() {
+            ParseQuery<Recipe> query = ParseQuery.getQuery("Recipe");
+            Recipe toRemove =  recipes.get(getAdapterPosition());
+            removeIngredients(toRemove);
+            recipes.get(getAdapterPosition()).deleteInBackground();
+            recipes.remove(recipes.get(getAdapterPosition()));
+            notifyDataSetChanged();
+        }
+
+        // requests the Parse database storing ingredients for the recipe
+        private void removeIngredients(Recipe recipe) {
+            JSONArray jsonArray = new JSONArray();
+            ParseQuery<Ingredient> query = ParseQuery.getQuery(Ingredient.class);
+            query.whereEqualTo("recipe", recipe);
+            query.addDescendingOrder("createdAt");
+            query.findInBackground(new FindCallback<Ingredient>() {
+                public void done(List<Ingredient> itemList, ParseException e) {
+                    if (e == null) {
+                        // Access the array of results here
+                        for (Ingredient item: itemList) {
+                            item.deleteInBackground();
+                        }
+                    } else {
+                        Log.d("item", "Error: " + e.getMessage());
+                    }
+                }
+            });
         }
 
         // Update the view inside of the holder with this data
@@ -65,5 +109,12 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder
             tvRecipe.setText(item.getTitle());
         }
 
+        // transitions to detail view and passes recipe
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(context, DetailActivity.class);
+            intent.putExtra("recipe", (Serializable) recipes.get(getAdapterPosition()));
+            context.startActivity(intent);
+        }
     }
 }
